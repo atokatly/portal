f3a154439e52ac884336938c1ddafae14ee68867
Anthony Tokatly <atokatly884@gmail.com>
2017-06-12 11:56:12 -0700

    add tokyo and western view files

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -5,9 +5,11 @@ class RoomController < ApplicationController
 
   def tokyo
     p 'This is the Tokyo route'
+    render "/rooms/tokyo"
   end
 
   def western
     p 'This is the Western route'
+    render "/rooms/western"
   end
 end
